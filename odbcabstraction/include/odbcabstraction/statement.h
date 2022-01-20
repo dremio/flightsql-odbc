bf11941d36b251c4b1533bd621b779341a33f14e
Rafael Telles <rafael.telles@dremio.com>
2022-01-20 18:03:18 -0300

    Fix code style issues on statement.h
    Change-Id: I1d3a4a5473d50db7f3fa8a1b6fc05abd5b7b475c

--- odbcabstraction/include/odbcabstraction/statement.h
+++ odbcabstraction/include/odbcabstraction/statement.h
@@ -48,7 +48,7 @@ public:
 
   /// \brief Set a statement attribute (may be called at any time)
   ///
-  ///NOTE: Meant to be bound with SQLSetStmtAttr.
+  /// NOTE: Meant to be bound with SQLSetStmtAttr.
   ///
   /// \param attribute Attribute identifier to set.
   /// \param value Value to be associated with the attribute.
@@ -61,14 +61,14 @@ public:
   ///
   /// \param attribute Attribute identifier to be retrieved.
   /// \return Value associated with the attribute.
-  virtual optional <Statement::Attribute>
+  virtual optional<Statement::Attribute>
   GetAttribute(Statement::StatementAttributeId attribute) = 0;
 
   /// \brief Prepares the statement.
   /// Returns ResultSetMetadata if query returns a result set,
   /// otherwise it returns `boost::none`.
   /// \param query The SQL query to prepare.
-  virtual boost::optional <std::shared_ptr<ResultSetMetadata>>
+  virtual boost::optional<std::shared_ptr<ResultSetMetadata>>
   Prepare(const std::string &query) = 0;
 
   /// \brief Execute the prepared statement.
@@ -87,31 +87,31 @@ public:
   virtual bool Execute(const std::string &query) = 0;
 
   /// \brief Returns the current result as a ResultSet object.
-  virtual std::shared_ptr <ResultSet> GetResultSet() = 0;
+  virtual std::shared_ptr<ResultSet> GetResultSet() = 0;
 
   /// \brief Retrieves the current result as an update count;
   /// if the result is a ResultSet object or there are no more results, -1 is
   /// returned.
   virtual long GetUpdateCount() = 0;
 
-  /// \brief Returns the list of table, catalog, or schema names, and table types,
-  /// stored in a specific data source. The driver returns the information as a
-  /// result set.
+  /// \brief Returns the list of table, catalog, or schema names, and table
+  /// types, stored in a specific data source. The driver returns the
+  /// information as a result set.
   ///
-  ///NOTE: This is meant to be used by ODBC 2.x binding.
+  /// NOTE: This is meant to be used by ODBC 2.x binding.
   ///
   /// \param catalog_name The catalog name.
   /// \param schema_name The schema name.
   /// \param table_name The table name.
   /// \param table_type The table type.
-  virtual std::shared_ptr <ResultSet>
+  virtual std::shared_ptr<ResultSet>
   GetTables_V2(const std::string *catalog_name, const std::string *schema_name,
                const std::string *table_name,
                const std::string *table_type) = 0;
 
-  /// \brief Returns the list of table, catalog, or schema names, and table types,
-  /// stored in a specific data source. The driver returns the information as a
-  /// result set.
+  /// \brief Returns the list of table, catalog, or schema names, and table
+  /// types, stored in a specific data source. The driver returns the
+  /// information as a result set.
   ///
   /// NOTE: This is meant to be used by ODBC 3.x binding.
   ///
@@ -119,13 +119,13 @@ public:
   /// \param schema_name The schema name.
   /// \param table_name The table name.
   /// \param table_type The table type.
-  virtual std::shared_ptr <ResultSet>
+  virtual std::shared_ptr<ResultSet>
   GetTables_V3(const std::string *catalog_name, const std::string *schema_name,
                const std::string *table_name,
                const std::string *table_type) = 0;
 
-  /// \brief Returns the list of column names in specified tables. The driver returns
-  /// this information as a result set..
+  /// \brief Returns the list of column names in specified tables. The driver
+  /// returns this information as a result set..
   ///
   /// NOTE: This is meant to be used by ODBC 2.x binding.
   ///
@@ -133,13 +133,13 @@ public:
   /// \param schema_name The schema name.
   /// \param table_name The table name.
   /// \param column_name The column name.
-  virtual std::shared_ptr <ResultSet>
+  virtual std::shared_ptr<ResultSet>
   GetColumns_V2(const std::string *catalog_name, const std::string *schema_name,
                 const std::string *table_name,
                 const std::string *column_name) = 0;
 
-  /// \brief Returns the list of column names in specified tables. The driver returns
-  /// this information as a result set..
+  /// \brief Returns the list of column names in specified tables. The driver
+  /// returns this information as a result set..
   ///
   /// NOTE: This is meant to be used by ODBC 3.x binding.
   ///
@@ -147,20 +147,21 @@ public:
   /// \param schema_name The schema name.
   /// \param table_name The table name.
   /// \param column_name The column name.
-  virtual std::shared_ptr <ResultSet>
+  virtual std::shared_ptr<ResultSet>
   GetColumns_V3(const std::string *catalog_name, const std::string *schema_name,
                 const std::string *table_name,
                 const std::string *column_name) = 0;
 
-  /// \brief Returns information about data types supported by the data source. The
-  /// driver returns the information in the form of an SQL result set. The data
-  /// types are intended for use in Data Definition Language (DDL) statements.
+  /// \brief Returns information about data types supported by the data source.
+  /// The driver returns the information in the form of an SQL result set. The
+  /// data types are intended for use in Data Definition Language (DDL)
+  /// statements.
   ///
   /// Accepts ODBC 2.x or 3.x data types as input.
   /// Reports results according to which ODBC version is set on Driver.
   ///
   /// \param dataType The SQL data type.
-  virtual std::shared_ptr <ResultSet> GetTypeInfo(int dataType) = 0;
+  virtual std::shared_ptr<ResultSet> GetTypeInfo(int dataType) = 0;
 };
 
 } // namespace odbcabstraction
