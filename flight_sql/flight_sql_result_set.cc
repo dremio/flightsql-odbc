5f10a40ffc85ac68a71f746d0a93de28442414b9
Rafael Telles <rafael.telles@dremio.com>
2022-02-22 16:08:35 -0300

    WIP: Add Arrow compute to simplify Accessors
    Change-Id: Ic03c88f3555419307fd3492e989a1154f9c7b471

--- flight_sql/flight_sql_result_set.cc
+++ flight_sql/flight_sql_result_set.cc
@@ -22,7 +22,7 @@
 #include <iostream>
 #include <utility>
 
-#include "flight_sql_result_set_accessors.h"
+#include "flight_sql_result_set_column.h"
 #include "flight_sql_result_set_metadata.h"
 #include "utils.h"
 
@@ -39,58 +39,19 @@ using arrow::flight::FlightStreamReader;
 using odbcabstraction::CDataType;
 using odbcabstraction::DriverException;
 
-FlightStreamChunkIterator::FlightStreamChunkIterator(
-    FlightSqlClient &flight_sql_client,
-    const flight::FlightCallOptions &call_options,
-    const std::shared_ptr<FlightInfo> &flight_info)
-    : closed_(false) {
-  const std::vector<FlightEndpoint> &vector = flight_info->endpoints();
-
-  stream_readers_.reserve(vector.size());
-  for (int i = 0; i < vector.size(); ++i) {
-    auto result = flight_sql_client.DoGet(call_options, vector[0].ticket);
-    ThrowIfNotOK(result.status());
-    stream_readers_.push_back(std::move(result.ValueOrDie()));
-  }
-
-  stream_readers_it_ = stream_readers_.begin();
-}
-
-FlightStreamChunkIterator::~FlightStreamChunkIterator() { Close(); }
-
-bool FlightStreamChunkIterator::GetNext(FlightStreamChunk *chunk) {
-  chunk->data = nullptr;
-  while (stream_readers_it_ != stream_readers_.end()) {
-    ThrowIfNotOK((*stream_readers_it_)->Next(chunk));
-    if (chunk->data != nullptr) {
-      return true;
-    }
-    stream_readers_it_++;
-  }
-  return false;
-}
-
-void FlightStreamChunkIterator::Close() {
-  if (closed_) {
-    return;
-  }
-  for (const auto &item : stream_readers_) {
-    item->Cancel();
-  }
-  closed_ = true;
-}
-
 FlightSqlResultSet::FlightSqlResultSet(
     std::shared_ptr<ResultSetMetadata> metadata,
     FlightSqlClient &flight_sql_client,
     const arrow::flight::FlightCallOptions &call_options,
     const std::shared_ptr<FlightInfo> &flight_info)
-    : metadata_(std::move(metadata)), binding_(metadata->GetColumnCount()),
-      accessors_(metadata->GetColumnCount()),
-      get_data_offsets_(metadata->GetColumnCount(), 0), current_row_(0),
-      num_binding_(0),
+    : metadata_(std::move(metadata)), columns_(metadata->GetColumnCount()),
+      current_row_(0), num_binding_(0),
       chunk_iterator_(flight_sql_client, call_options, flight_info) {
   current_chunk_.data = nullptr;
+
+  for (int i = 0; i < metadata_->GetColumnCount(); ++i) {
+    columns_[i].reset(new FlightSqlResultSetColumn(this, i + 1));
+  }
 
   ThrowIfNotOK(flight_info->GetSchema(nullptr, &schema_));
 }
@@ -106,8 +67,10 @@ size_t FlightSqlResultSet::Move(size_t rows) {
   }
 
   // Reset GetData value offsets.
-  if (num_binding_ != get_data_offsets_.size()) {
-    std::fill(get_data_offsets_.begin(), get_data_offsets_.end(), 0);
+  if (num_binding_ != columns_.size()) {
+    for (auto &column : columns_) {
+      column->get_data_offset = 0;
+    }
   }
 
   size_t fetched_rows = 0;
@@ -121,19 +84,23 @@ size_t FlightSqlResultSet::Move(size_t rows) {
       if (!chunk_iterator_.GetNext(&current_chunk_)) {
         break;
       }
+      for (const auto &column : columns_) {
+        column->ResetAccessor();
+      }
       current_row_ = 0;
       continue;
     }
 
-    for (auto it = binding_.begin(); it != binding_.end(); *it++) {
+    for (auto it = columns_.begin(); it != columns_.end(); *it++) {
+      auto &column = *it;
+
       // There can be unbound columns.
-      ColumnBinding *binding = (*it).get();
+      ColumnBinding *binding = column->binding.get();
       if (binding == nullptr)
         continue;
 
-      Accessor *accessor = accessors_[binding->column - 1].get();
-      size_t accessor_rows = accessor->GetColumnarData(
-          this, binding, current_row_, rows_to_fetch, 0);
+      size_t accessor_rows = column->GetAccessorForBinding()->GetColumnarData(
+          binding, current_row_, rows_to_fetch, 0);
 
       if (rows_to_fetch != accessor_rows) {
         throw DriverException(
@@ -141,7 +108,7 @@ size_t FlightSqlResultSet::Move(size_t rows) {
       }
     }
 
-    current_row_ += rows_to_fetch;
+    current_row_ += static_cast<int64_t>(rows_to_fetch);
     fetched_rows += rows_to_fetch;
   }
 
@@ -153,24 +120,21 @@ void FlightSqlResultSet::Close() {
   current_chunk_.data = nullptr;
 }
 
-bool FlightSqlResultSet::GetData(int column, CDataType target_type,
+bool FlightSqlResultSet::GetData(int column_n, CDataType target_type,
                                  int precision, int scale, void *buffer,
                                  size_t buffer_length, ssize_t *strlen_buffer) {
-  ColumnBinding binding(column, target_type, precision, scale, buffer,
+  ColumnBinding binding(column_n, target_type, precision, scale, buffer,
                         buffer_length, strlen_buffer);
 
-  Accessor *accessor = accessors_[column - 1].get();
-  if (accessor == nullptr || accessor->GetTargetType() != target_type) {
-    accessors_[column - 1] = CreateAccessorForColumn(column, target_type);
-    accessor = accessors_[column - 1].get();
-  }
+  auto &column = columns_[column_n - 1];
+  Accessor *accessor = column->GetAccessorForGetData(target_type);
 
-  int64_t *value_offset = &get_data_offsets_[column - 1];
+  int64_t *value_offset = &column->get_data_offset;
 
   // Note: current_row_ is always positioned at the index _after_ the one we are
   // on after calling Move(). So if we want to get data from the _last_ row
   // fetched, we need to subtract one from the current row.
-  accessor->GetColumnarData(this, &binding, current_row_ - 1, 1, *value_offset);
+  accessor->GetColumnarData(&binding, current_row_ - 1, 1, *value_offset);
 
   if (strlen_buffer) {
     bool has_more = *value_offset + buffer_length <= strlen_buffer[0];
@@ -183,44 +147,36 @@ bool FlightSqlResultSet::GetData(int column, CDataType target_type,
 
 std::shared_ptr<arrow::Array>
 FlightSqlResultSet::GetArrayForColumn(int column) {
-  std::shared_ptr<Array> result = current_chunk_.data->column(column - 1);
-  return result;
+  std::shared_ptr<Array> original_array =
+      current_chunk_.data->column(column - 1);
+
+  return original_array;
 }
 
 std::shared_ptr<ResultSetMetadata> FlightSqlResultSet::GetMetadata() {
   return metadata_;
 }
 
-void FlightSqlResultSet::BindColumn(int column, CDataType target_type,
+void FlightSqlResultSet::BindColumn(int column_n, CDataType target_type,
                                     int precision, int scale, void *buffer,
                                     size_t buffer_length,
                                     ssize_t *strlen_buffer) {
+  auto &column = columns_[column_n - 1];
   if (buffer == nullptr) {
-    if (accessors_[column - 1] != nullptr) {
+    if (column->binding != nullptr) {
       num_binding_--;
     }
-    binding_[column - 1].reset();
-    accessors_[column - 1].reset();
+    column->SetBinding(nullptr);
     return;
   }
 
-  binding_[column - 1].reset(new ColumnBinding(column, target_type, precision,
-                                               scale, buffer, buffer_length,
-                                               strlen_buffer));
-  if (accessors_[column - 1] == nullptr) {
+  if (column->binding == nullptr) {
     num_binding_++;
   }
-  accessors_[column - 1] = CreateAccessorForColumn(column, target_type);
-}
-
-std::unique_ptr<Accessor>
-FlightSqlResultSet::CreateAccessorForColumn(int column, CDataType target_type) {
-  const std::shared_ptr<arrow::DataType> &source_type =
-      schema_->field(column - 1)->type();
-  return CreateAccessor(*source_type, target_type);
+  column->SetBinding(new ColumnBinding(column_n, target_type, precision, scale,
+                                       buffer, buffer_length, strlen_buffer));
 }
 
 FlightSqlResultSet::~FlightSqlResultSet() = default;
-
 } // namespace flight_sql
 } // namespace driver
