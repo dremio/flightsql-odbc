f45d5634e6b046ee05f4778c8df1bc1fe75f30d8
Rafael Telles <rafael.telles@dremio.com>
2022-02-22 16:08:35 -0300

    Add Arrow compute to simplify Accessors
    This enables using Arrow Compute to efficiently cast Arrow arrays and
    also removes much data conversion work that would be needed otherwise.
    
    Change-Id: Ic03c88f3555419307fd3492e989a1154f9c7b471

--- flight_sql/accessors/types.h
+++ flight_sql/accessors/types.h
@@ -17,29 +17,35 @@
 
 #pragma once
 
+#include <arrow/array.h>
 #include <cstddef>
 #include <cstdint>
+#include <odbcabstraction/exceptions.h>
 #include <odbcabstraction/types.h>
 #include <sstream>
 
 namespace driver {
 namespace flight_sql {
 
+using arrow::Array;
 using odbcabstraction::CDataType;
+
+class FlightSqlResultSet;
 
 struct ColumnBinding {
   void *buffer;
   ssize_t *strlen_buffer;
   size_t buffer_length;
   CDataType target_type;
-  int column;
   int precision;
   int scale;
 
-  ColumnBinding(int column, CDataType target_type, int precision, int scale,
-                void *buffer, size_t buffer_length, ssize_t *strlen_buffer)
-      : column(column), target_type(target_type), precision(precision),
-        scale(scale), buffer(buffer), buffer_length(buffer_length),
+  ColumnBinding() = default;
+
+  ColumnBinding(CDataType target_type, int precision, int scale, void *buffer,
+                size_t buffer_length, ssize_t *strlen_buffer)
+      : target_type(target_type), precision(precision), scale(scale),
+        buffer(buffer), buffer_length(buffer_length),
         strlen_buffer(strlen_buffer) {}
 };
 
@@ -47,14 +53,33 @@ struct ColumnBinding {
 /// single column to buffers bound by `ColumnarResultSet::BindColumn`.
 class Accessor {
 public:
+  const CDataType target_type_;
+
+  Accessor(CDataType target_type) : target_type_(target_type) {}
+
   virtual ~Accessor() = default;
 
-  virtual CDataType GetTargetType() = 0;
-
   /// \brief Populates next cells
-  virtual size_t GetColumnarData(FlightSqlResultSet *result_set,
-                                 ColumnBinding *binding, int64_t starting_row,
+  virtual size_t GetColumnarData(ColumnBinding *binding, int64_t starting_row,
                                  size_t cells, int64_t value_offset) = 0;
+};
+
+template <typename ARROW_ARRAY, CDataType TARGET_TYPE>
+class FlightSqlAccessor : public Accessor {
+public:
+  explicit FlightSqlAccessor(Array *array);
+
+  size_t GetColumnarData(ColumnBinding *binding, int64_t starting_row,
+                         size_t cells, int64_t value_offset) override;
+
+private:
+  ARROW_ARRAY *array_;
+
+  size_t GetColumnarData(const std::shared_ptr<ARROW_ARRAY> &sliced_array,
+                         ColumnBinding *binding, int64_t value_offset);
+
+  inline void MoveSingleCell(ColumnBinding *binding, ARROW_ARRAY *array,
+                             int64_t i, int64_t value_offset);
 };
 
 } // namespace flight_sql
---
+++ flight_sql/flight_sql_stream_chunk_iterator.cc
@@ -0,0 +1,68 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "flight_sql_stream_chunk_iterator.h"
+#include "utils.h"
+
+namespace driver {
+namespace flight_sql {
+
+using arrow::flight::FlightEndpoint;
+
+FlightStreamChunkIterator::FlightStreamChunkIterator(
+    FlightSqlClient &flight_sql_client,
+    const arrow::flight::FlightCallOptions &call_options,
+    const std::shared_ptr<FlightInfo> &flight_info)
+    : closed_(false) {
+  const std::vector<FlightEndpoint> &vector = flight_info->endpoints();
+
+  stream_readers_.reserve(vector.size());
+  for (int i = 0; i < vector.size(); ++i) {
+    auto result = flight_sql_client.DoGet(call_options, vector[0].ticket);
+    ThrowIfNotOK(result.status());
+    stream_readers_.push_back(std::move(result.ValueOrDie()));
+  }
+
+  stream_readers_it_ = stream_readers_.begin();
+}
+
+FlightStreamChunkIterator::~FlightStreamChunkIterator() { Close(); }
+
+bool FlightStreamChunkIterator::GetNext(FlightStreamChunk *chunk) {
+  chunk->data = nullptr;
+  while (stream_readers_it_ != stream_readers_.end()) {
+    ThrowIfNotOK((*stream_readers_it_)->Next(chunk));
+    if (chunk->data != nullptr) {
+      return true;
+    }
+    stream_readers_it_++;
+  }
+  return false;
+}
+
+void FlightStreamChunkIterator::Close() {
+  if (closed_) {
+    return;
+  }
+  for (const auto &item : stream_readers_) {
+    item->Cancel();
+  }
+  closed_ = true;
+}
+
+} // namespace flight_sql
+} // namespace driver
