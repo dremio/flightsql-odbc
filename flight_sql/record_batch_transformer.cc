d15b5d8cd75b42805f3b2fef5e3f0d623368a3f1
Jose Almeida <almeidajcr90@gmail.com>
2022-03-08 16:55:56 -0300

    DX-45556: Design a transformer that will change a RecordBatch according to parameters
    It implements a new class RecordBatchTransformer with its Builder. It is responsible to convert the name of fields
       inside a RecordBatch. These fields are changed based on tasks created by the methods RenameRecord() and AddEmptyFields(). The execution of the tasks is handled by the method transformer.
    
    Change-Id: I8894d0cbfc21b11aa6c64c9ffaab92dcedff0724

---
+++ flight_sql/record_batch_transformer.cc
@@ -0,0 +1,127 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "record_batch_transformer.h"
+
+#include "utils.h"
+#include <arrow/array/util.h>
+#include <arrow/testing/gtest_util.h>
+#include <iostream>
+#include <utility>
+
+#include "arrow/array/array_base.h"
+
+using namespace arrow;
+
+namespace driver {
+namespace flight_sql {
+
+namespace {
+Result<std::shared_ptr<Array>> MakeEmptyArray(std::shared_ptr<DataType> type,
+                                              MemoryPool *memory_pool) {
+  std::unique_ptr<ArrayBuilder> builder;
+  RETURN_NOT_OK(MakeBuilder(memory_pool, type, &builder));
+  RETURN_NOT_OK(builder->Resize(0));
+  return builder->Finish();
+}
+} // namespace
+
+RecordBatchTransformer::Builder &RecordBatchTransformer::Builder::RenameRecord(
+    const std::string &originalName, const std::string &transformedName) {
+
+  auto pFunction =
+      [&originalName, &transformedName](
+          const std::shared_ptr<arrow::RecordBatch> &originalRecord,
+          const std::shared_ptr<arrow::Schema> &transformedSchema) {
+        auto originalDataType =
+            originalRecord->schema()->GetFieldByName(originalName);
+        auto transformedDataType =
+            transformedSchema->GetFieldByName(transformedName);
+
+        if (originalDataType->type() != transformedDataType->type()) {
+          throw odbcabstraction::DriverException(
+              "Original data and target data has different types");
+        }
+
+        return originalRecord->GetColumnByName(originalName);
+      };
+
+  task_collection_.emplace_back(pFunction);
+
+  auto originalFields = schema_->GetFieldByName(originalName);
+
+  new_fields_.push_back(arrow::field(transformedName, originalFields->type(),
+                                     originalFields->metadata()));
+
+  return *this;
+}
+
+RecordBatchTransformer::Builder &
+RecordBatchTransformer::Builder::AddEmptyFields(
+    const std::string &fieldName,
+    const std::shared_ptr<arrow::DataType> &dataType) {
+  auto pFunction =
+      [=](const std::shared_ptr<arrow::RecordBatch> &originalRecord,
+          const std::shared_ptr<arrow::Schema> &transformedSchema) {
+        auto result = MakeEmptyArray(dataType, nullptr);
+        driver::flight_sql::ThrowIfNotOK(result.status());
+
+        return result.ValueOrDie();
+      };
+
+  task_collection_.emplace_back(pFunction);
+
+  new_fields_.push_back(arrow::field(fieldName, dataType));
+
+  return *this;
+}
+
+RecordBatchTransformer RecordBatchTransformer::Builder::Build() {
+  RecordBatchTransformer transformer(*this);
+
+  return transformer;
+}
+
+RecordBatchTransformer::Builder::Builder(std::shared_ptr<arrow::Schema> schema)
+    : schema_(std::move(schema)) {}
+
+  std::shared_ptr<arrow::RecordBatch> RecordBatchTransformer::Transform(
+    const std::shared_ptr<arrow::RecordBatch> &original) {
+  auto newSchema = arrow::schema(fields_);
+
+  std::vector<std::shared_ptr<arrow::Array>> arrays;
+
+  for (const auto &item : tasks_) {
+    arrays.push_back(item(original, newSchema));
+  }
+
+  auto transformedBatch =
+      arrow::RecordBatch::Make(newSchema, original->num_rows(), arrays);
+  return transformedBatch;
+}
+
+std::shared_ptr<arrow::Schema> RecordBatchTransformer::GetTransformedSchema() {
+  return arrow::schema(fields_);
+}
+
+RecordBatchTransformer::RecordBatchTransformer(
+  RecordBatchTransformer::Builder& builder)  {
+  this->fields_.swap(builder.new_fields_);
+  this->tasks_.swap(builder.task_collection_);
+}
+} // namespace flight_sql
+} // namespace driver
