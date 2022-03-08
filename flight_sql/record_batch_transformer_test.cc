b36fd2cb1052431a2ae18257c1e63af26435c23e
Jose Almeida <almeidajcr90@gmail.com>
2022-03-08 16:55:56 -0300

    Implement record batch transformer
    Change-Id: I8894d0cbfc21b11aa6c64c9ffaab92dcedff0724

---
+++ flight_sql/record_batch_transformer_test.cc
@@ -0,0 +1,54 @@
+#include "record_batch_transformer.h"
+#include "gtest/gtest.h"
+#include <arrow/testing/gtest_util.h>
+#include <arrow/record_batch.h>
+
+TEST(Transformer, TransformerTest) {
+  std::vector<int> values = {1,2,3,4,5};
+  std::shared_ptr<arrow::Array> array;
+
+  arrow::ArrayFromVector<arrow::Int32Type, int32_t>(values, &array);
+
+  auto schema = arrow::schema({arrow::field("test", arrow::int32(), false)});
+
+  auto originalRecord = arrow::RecordBatch::Make(schema, 4, {array});
+
+  std::string originalName("test");
+  std::string transformed("test1");
+
+  auto transformer = RecordBatchTransformer::Builder(schema)
+    .renameRecord(originalName, transformed)
+    .Build();
+
+  const std::shared_ptr<arrow::RecordBatch> &transformedBatch = transformer.transform(
+    originalRecord);
+
+  ASSERT_NE(originalRecord->schema(), transformedBatch->schema());
+  ASSERT_EQ(originalRecord->GetColumnByName(originalName), transformedBatch->GetColumnByName(transformed));
+}
+
+
+TEST(Transformer, TransformerAddEmptyVectorTest) {
+  std::vector<int> values = {1,2,3,4,5};
+  std::shared_ptr<arrow::Array> array;
+
+  arrow::ArrayFromVector<arrow::Int32Type, int32_t>(values, &array);
+  
+  auto schema = arrow::schema({arrow::field("test", arrow::int32(), false)});
+
+  auto originalBatch = arrow::RecordBatch::Make(schema, 4, {array});
+
+  std::string originalName("test");
+  std::string transformed("test1");
+
+  auto transformer = RecordBatchTransformer::Builder(schema)
+    .renameRecord(originalName, transformed)
+    .addEmptyFields(std::string("empty"), arrow::int32())
+    .Build();
+
+  const std::shared_ptr<arrow::RecordBatch> &transformedBatch = transformer.transform(
+    originalBatch);
+
+  ASSERT_NE(originalBatch->schema(), transformedBatch->schema());
+  ASSERT_EQ(originalBatch->GetColumnByName(originalName), transformedBatch->GetColumnByName(transformed));
+}
