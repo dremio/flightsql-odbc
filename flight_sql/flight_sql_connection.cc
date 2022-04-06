953e25af5c7d434f7bdd1ec24d49f202c92fd668
Alex McRae <alex.mcrae@dremio.com>
2022-04-06 14:12:43 -0700

    Add token authentication
    Change-Id: I9f0cafcada5195a75f3b262eeb0a51561d2601b1

--- flight_sql/flight_sql_connection.cc
+++ flight_sql/flight_sql_connection.cc
@@ -23,6 +23,7 @@
 #include <boost/optional.hpp>
 #include <odbcabstraction/exceptions.h>
 #include <sqlext.h>
+#include <iostream>
 
 #include "flight_sql_auth_method.h"
 #include "flight_sql_statement.h"
@@ -50,6 +51,7 @@ const std::string FlightSqlConnection::USER = "user";
 const std::string FlightSqlConnection::UID = "uid";
 const std::string FlightSqlConnection::PASSWORD = "password";
 const std::string FlightSqlConnection::PWD = "pwd";
+const std::string FlightSqlConnection::TOKEN = "token";
 const std::string FlightSqlConnection::USE_TLS = "useTls";
 
 namespace {
