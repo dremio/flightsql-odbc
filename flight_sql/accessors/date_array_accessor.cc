cbfbd34ed861d3b1dda8e833996ae3518b6fb082
Jose Almeida <jose.almeida@dremio.com>
2022-04-13 11:48:02 -0300

    Implements accessor for date, time and timestamp
    - Add an accessor to handle date32 and date64 arrays
    - Add an accessor to handle timestamp arrays handling their time units
    - Add an accessor to handle time array handling their time units
    - Instantiate the accessor on the create method
    - Add tests for the created accessors
    
    Change-Id: Ib4dbb9a4c5ad7d4d8b261fd975992cf2cd4cd13c

---
+++ flight_sql/accessors/date_array_accessor.cc
@@ -0,0 +1,81 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+#include "date_array_accessor.h"
+#include "time.h"
+#include "arrow/compute/api.h"
+#include "calendar_utils.h"
+
+namespace driver {
+namespace flight_sql {
+namespace {
+template <typename T> long convertDate(typename T::value_type value) {
+  return value;
+}
+
+/// Converts the value from the array, which is in milliseconds, to seconds.
+/// \param value    the value extracted from the array in milliseconds.
+/// \return         the converted value in seconds.
+template <> long convertDate<Date64Array>(int64_t value) {
+  return value / MILLI_TO_SECONDS_DIVISOR;
+}
+
+/// Converts the value from the array, which is in days, to seconds.
+/// \param value    the value extracted from the array in days.
+/// \return         the converted value in seconds.
+template <> long convertDate<Date32Array>(int32_t value) {
+  return value * DAYS_TO_SECONDS_MULTIPLIER;
+}
+} // namespace
+
+using namespace arrow;
+using namespace odbcabstraction;
+
+template <CDataType TARGET_TYPE, typename ARROW_ARRAY>
+DateArrayFlightSqlAccessor<
+    TARGET_TYPE, ARROW_ARRAY>::DateArrayFlightSqlAccessor(Array *array)
+    : FlightSqlAccessor<ARROW_ARRAY, TARGET_TYPE,
+                        DateArrayFlightSqlAccessor<TARGET_TYPE, ARROW_ARRAY>>(
+          array) {}
+
+template <CDataType TARGET_TYPE, typename ARROW_ARRAY>
+void DateArrayFlightSqlAccessor<TARGET_TYPE, ARROW_ARRAY>::MoveSingleCell_impl(
+    ColumnBinding *binding, ARROW_ARRAY *array, int64_t i,
+    int64_t value_offset) {
+  typedef unsigned char c_type;
+  auto *buffer = static_cast<DATE_STRUCT *>(binding->buffer);
+  long value = convertDate<ARROW_ARRAY>(array->Value(i));
+  static thread_local tm date;
+
+  GetTimeForAccessor(date, value);
+
+  buffer[i].year = 1900 + (date.tm_year);
+  buffer[i].month = date.tm_mon + 1;
+  buffer[i].day = date.tm_mday;
+
+  if (binding->strlen_buffer) {
+    binding->strlen_buffer[i] = static_cast<ssize_t>(sizeof(DATE_STRUCT));
+  }
+}
+
+template class DateArrayFlightSqlAccessor<odbcabstraction::CDataType_DATE,
+                                          Date32Array>;
+template class DateArrayFlightSqlAccessor<odbcabstraction::CDataType_DATE,
+                                          Date64Array>;
+
+} // namespace flight_sql
+} // namespace driver
