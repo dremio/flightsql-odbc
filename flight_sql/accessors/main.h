45bda191e80b9bad3c4042650e1fdbdb74d9b07e
Rafael Telles <rafael.telles@dremio.com>
2022-05-09 14:21:37 -0300

    Decimal array accessor
    Change-Id: I1c7738b77a269b8d6c9a423c00bd0200272e7d8e

--- flight_sql/accessors/main.h
+++ flight_sql/accessors/main.h
@@ -19,5 +19,6 @@
 
 #include "binary_array_accessor.h"
 #include "boolean_array_accessor.h"
+#include "decimal_array_accessor.h"
 #include "primitive_array_accessor.h"
 #include "string_array_accessor.h"
