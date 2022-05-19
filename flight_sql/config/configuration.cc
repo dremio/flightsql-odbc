e29540c7825d9e9d731e39e2b7759022c98cb39b
joy.cundy <joy.cundy@dremio.com>
2022-05-19 04:19:49 +0000

    [WIP] Implement Configuration Dialog
    Change-Id: If7d496734275c19d94fdf1d6070dfab640061401

---
+++ flight_sql/config/configuration.cc
@@ -0,0 +1,186 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "config/configuration.h"
+
+#include <sstream>
+#include <iterator>
+#include <odbcinst.h>
+#include <boost/range/adaptor/map.hpp>
+#include <boost/range/algorithm/copy.hpp>
+
+namespace driver {
+namespace flight_sql {
+namespace config {
+
+const std::string Key::DSN = "dsn";
+const std::string Key::DRIVER = "driver";
+const std::string Key::HOST = "host";
+const std::string Key::PORT = "port";
+const std::string Key::AUTH_TOKEN = "token";
+const std::string Key::UID = "uid";
+const std::string Key::PWD = "pwd";
+const std::string Key::USE_TLS = "UseEncryption";
+const std::string Key::ENCRYPTION_CERT = "TrustedCerts";
+const std::string Key::USE_CERT_STORE = "UseSystemTrustStore";
+const std::string Key::DISABLE_CERT_VERIFICATION = "DisableCertificateVerification";
+static const std::vector<std::string> ALL_KEYS = { 
+    Key::DSN, Key::DRIVER, Key::HOST, Key::PORT, Key::AUTH_TOKEN, Key::UID, Key::PWD, Key::USE_TLS, 
+    Key::ENCRYPTION_CERT, Key::USE_CERT_STORE, Key::DISABLE_CERT_VERIFICATION };
+
+static const std::string DEFAULT_DSN = "Apache Arrow Flight SQL";
+static const std::string DEFAULT_ENABLE_ENCRYPTION = TRUE_STR;
+static const std::string DEFAULT_USE_CERT_STORE = TRUE_STR;
+static const std::string DEFAULT_DISABLE_CERT_VERIFICATION = FALSE_STR;
+
+namespace {
+std::string ReadDsnString(const std::string& dsn, const std::string& key, const std::string& dflt = "")
+{
+    #define BUFFER_SIZE (1024)
+    std::vector<char> buf(BUFFER_SIZE);
+
+    int ret = SQLGetPrivateProfileString(dsn.c_str(), key.c_str(), dflt.c_str(), buf.data(), static_cast<int>(buf.size()), "ODBC.INI");
+
+    if (ret > BUFFER_SIZE)
+    {
+        // If there wasn't enough space, try again with the right size buffer.
+        buf.resize(ret + 1);
+        ret = SQLGetPrivateProfileString(dsn.c_str(), key.c_str(), dflt.c_str(), buf.data(), static_cast<int>(buf.size()), "ODBC.INI");
+    }
+
+    return std::string(buf.data(), ret);
+}
+
+void RemoveAllKnownKeys(std::vector<std::string>& keys) {
+    // Remove all known DSN keys from the passed in set of keys, case insensitively.
+    keys.erase(std::remove_if(keys.begin(), keys.end(), [&](auto& x) {
+        return std::find_if(ALL_KEYS.begin(), ALL_KEYS.end(), [&](auto& s) {return boost::iequals(x, s);}) != ALL_KEYS.end();
+        }), keys.end());
+}
+
+std::vector<std::string> ReadAllKeys(const std::string& dsn)
+{
+    std::vector<char> buf(BUFFER_SIZE);
+
+    int ret = SQLGetPrivateProfileString(dsn.c_str(), NULL, "", buf.data(), static_cast<int>(buf.size()), "ODBC.INI");
+
+    if (ret > BUFFER_SIZE)
+    {
+        // If there wasn't enough space, try again with the right size buffer.
+        buf.resize(ret + 1);
+        ret = SQLGetPrivateProfileString(dsn.c_str(), NULL, "", buf.data(), static_cast<int>(buf.size()), "ODBC.INI");
+    }
+
+    // When you pass NULL to SQLGetPrivateProfileString it gives back a \0 delimited list of all the keys.
+    // The below loop simply tokenizes all the keys and places them into a vector.
+    std::vector<std::string> keys;
+    char* begin = buf.data();
+    while (begin && *begin != '\0') {
+        char* cur;
+        for (cur = begin; *cur != '\0'; ++cur);
+        keys.emplace_back(begin, cur);
+        begin = ++cur;
+    }
+    return keys;
+}
+}
+
+Configuration::Configuration()
+{
+    // No-op.
+}
+
+Configuration::~Configuration()
+{
+    // No-op.
+}
+
+void Configuration::LoadDefaults()
+{
+    Set(Key::DSN, DEFAULT_DSN);
+    Set(Key::USE_TLS, DEFAULT_ENABLE_ENCRYPTION);
+    Set(Key::USE_CERT_STORE, DEFAULT_USE_CERT_STORE);
+    Set(Key::DISABLE_CERT_VERIFICATION, DEFAULT_DISABLE_CERT_VERIFICATION);
+}
+
+void Configuration::LoadDsn(const std::string& dsn)
+{
+    Set(Key::DSN, dsn);
+    Set(Key::HOST, ReadDsnString(dsn, Key::HOST));
+    Set(Key::PORT, ReadDsnString(dsn, Key::PORT));
+    Set(Key::AUTH_TOKEN, ReadDsnString(dsn, Key::AUTH_TOKEN));
+    Set(Key::UID, ReadDsnString(dsn, Key::UID));
+    Set(Key::PWD, ReadDsnString(dsn, Key::PWD));
+    Set(Key::USE_TLS, ReadDsnString(dsn, Key::USE_TLS, DEFAULT_ENABLE_ENCRYPTION));
+    Set(Key::ENCRYPTION_CERT, ReadDsnString(dsn, Key::ENCRYPTION_CERT));
+    Set(Key::USE_CERT_STORE, ReadDsnString(dsn, Key::USE_CERT_STORE, DEFAULT_USE_CERT_STORE));
+    Set(Key::DISABLE_CERT_VERIFICATION, ReadDsnString(dsn, Key::DISABLE_CERT_VERIFICATION, DEFAULT_DISABLE_CERT_VERIFICATION));
+
+    auto customKeys = ReadAllKeys(dsn);
+    RemoveAllKnownKeys(customKeys);
+    for (auto key : customKeys) {
+        Set(key, ReadDsnString(dsn, key));
+    }
+}
+
+void Configuration::Clear()
+{
+    this->properties.clear();
+}
+
+bool Configuration::IsSet(const std::string& key) const
+{
+    return 0 != this->properties.count(key);
+}
+
+const std::string& Configuration::Get(const std::string& key) const
+{
+    const auto itr = this->properties.find(key);
+    if (itr == this->properties.cend()) {
+        static const std::string empty("");
+        return empty;
+    }
+    return itr->second;
+}
+
+void Configuration::Set(const std::string& key, const std::string& value)
+{
+    const std::string copy = boost::trim_copy(value);
+    if (!copy.empty()) {
+        this->properties[key] = value;
+    }
+}
+
+const driver::odbcabstraction::Connection::ConnPropertyMap& Configuration::GetProperties() const
+{
+    return this->properties;
+}
+
+std::vector<std::string> Configuration::GetCustomKeys() const
+{
+    driver::odbcabstraction::Connection::ConnPropertyMap copyProps(properties);
+    for (auto& key : ALL_KEYS) {
+        copyProps.erase(key);
+    }
+    std::vector<std::string> keys;
+    boost::copy(copyProps | boost::adaptors::map_keys, std::back_inserter(keys));
+    return keys;
+}
+
+} // namespace config
+} // namespace flight_sql
+} // namespace driver
