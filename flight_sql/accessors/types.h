231f904a6661c9433153f62f4ba64986360dc729
Rafael Telles <rafael.telles@dremio.com>
2022-06-07 17:53:24 -0300

    WIP: Add row status array
    Change-Id: I85590c766daac7510aca68e667c7a4a907193f73

--- flight_sql/accessors/types.h
+++ flight_sql/accessors/types.h
@@ -51,7 +51,7 @@ public:
   virtual ~Accessor() = default;
 
   /// \brief Populates next cells
-  virtual size_t GetColumnarData(ColumnBinding *binding, int64_t starting_row,
+  virtual size_t GetColumnarData(ColumnBinding *binding, uint16_t* row_status_array, int64_t starting_row,
                                  size_t cells, int64_t &value_offset, bool update_value_offset,
                                  odbcabstraction::Diagnostics &diagnostics) = 0;
 
@@ -65,7 +65,7 @@ public:
       : Accessor(TARGET_TYPE),
         array_(arrow::internal::checked_cast<ARROW_ARRAY *>(array)) {}
 
-  size_t GetColumnarData(ColumnBinding *binding, int64_t starting_row,
+  size_t GetColumnarData(ColumnBinding *binding, uint16_t* row_status_array, int64_t starting_row,
                          size_t cells, int64_t &value_offset, bool update_value_offset,
                          odbcabstraction::Diagnostics &diagnostics) override {
     const std::shared_ptr<Array> &array =
@@ -73,7 +73,7 @@ public:
 
     return GetColumnarData(
         arrow::internal::checked_pointer_cast<ARROW_ARRAY>(array), binding,
-        value_offset, update_value_offset, diagnostics);
+        row_status_array, value_offset, update_value_offset, diagnostics);
   }
 
   size_t GetCellLength(ColumnBinding *binding) const override {
@@ -84,15 +84,15 @@ private:
   ARROW_ARRAY *array_;
 
   size_t GetColumnarData(const std::shared_ptr<ARROW_ARRAY> &sliced_array,
-                         ColumnBinding *binding, int64_t &value_offset, bool update_value_offset,
+                         ColumnBinding *binding, uint16_t* row_status_array, int64_t &value_offset, bool update_value_offset,
                          odbcabstraction::Diagnostics &diagnostics) {
     return static_cast<DERIVED *>(this)->GetColumnarData_impl(
-        sliced_array, binding, value_offset, update_value_offset, diagnostics);
+        sliced_array, binding, value_offset, update_value_offset, diagnostics, row_status_array);
   }
 
   size_t GetColumnarData_impl(const std::shared_ptr<ARROW_ARRAY> &sliced_array,
                               ColumnBinding *binding, int64_t &value_offset, bool update_value_offset,
-                              odbcabstraction::Diagnostics &diagnostics) {
+                              odbcabstraction::Diagnostics &diagnostics, uint16_t* row_status_array) {
     int64_t length = sliced_array->length();
     for (int64_t i = 0; i < length; ++i) {
       if (sliced_array->IsNull(i)) {
@@ -104,8 +104,14 @@ private:
       } else {
         // TODO: Optimize this by creating different versions of MoveSingleCell
         // depending on if strlen_buffer is null.
-        MoveSingleCell(binding, sliced_array.get(), i, value_offset, update_value_offset,
-                       diagnostics);
+        try {
+          MoveSingleCell(binding, sliced_array.get(), i, value_offset, update_value_offset,
+                         diagnostics);
+          row_status_array[i] = 0;
+        } catch (...) {
+          // TODO: Should we throw?
+          row_status_array[i] = 5;
+        }
       }
     }
 
