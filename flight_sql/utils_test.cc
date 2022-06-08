3c011e4f5624fa3a470855e54905be0d0ea643f1
Jose Almeida <jose.almeida@dremio.com>
2022-06-08 10:58:44 -0300

    Add time to timestamp converter.
    - This patch fix a conversion error that happens when trying to fetch a time data as timestamp.
    This error could be reproduced when making the explicit conversion or using pyodbc to query a time column
    
    Change-Id: I075376bedcef6b0d36101cf2105973696f4d5f35

--- flight_sql/utils_test.cc
+++ flight_sql/utils_test.cc
@@ -5,40 +5,131 @@
  */
 
 #include "utils.h"
+
+#include "odbcabstraction/calendar_utils.h"
+
 #include "arrow/testing/builder.h"
-#include <odbcabstraction/platform.h> 
+#include "arrow/testing/gtest_util.h"
+#include "arrow/testing/util.h"
 #include "gtest/gtest.h"
 
 namespace driver {
 namespace flight_sql {
 
+void AssertConvertedArray(const std::shared_ptr<arrow::Array>& expected_array,
+                          const std::shared_ptr<arrow::Array>& converted_array,
+                          uint64_t size,
+                          arrow::Type::type arrow_type) {
+  ASSERT_EQ(converted_array->type_id(), arrow_type);
+  ASSERT_EQ(converted_array->length(),size);
+  ASSERT_EQ(expected_array->ToString(), converted_array->ToString());
+}
+
+std::shared_ptr<arrow::Array> convertArray(
+  const std::shared_ptr<arrow::Array>& original_array,
+  odbcabstraction::CDataType c_type) {
+  auto converter = GetConverter(original_array->type_id(),
+                                c_type);
+  return converter(original_array);
+}
+
 void TestArrayConversion(const std::vector<std::string>& input,
+                         const std::shared_ptr<arrow::Array>& expected_array,
                          odbcabstraction::CDataType c_type,
                          arrow::Type::type arrow_type) {
-  std::shared_ptr<arrow::Array> array;
-  arrow::ArrayFromVector<arrow::StringType, std::string>(input, &array);
+  std::shared_ptr<arrow::Array> original_array;
+  arrow::ArrayFromVector<arrow::StringType, std::string>(input, &original_array);
 
-  auto converter = GetConverter(array->type_id(),
-                                c_type);
-  auto converted_array = converter(array);
+  auto converted_array = convertArray(original_array, c_type);
 
-  ASSERT_EQ(converted_array->type_id(), arrow_type);
-  ASSERT_EQ(converted_array->length(), input.size());
+  AssertConvertedArray(expected_array, converted_array, input.size(), arrow_type);
+}
+
+void TestTime32ArrayConversion(const std::vector<int32_t>& input,
+                               const std::shared_ptr<arrow::Array>& expected_array,
+                               odbcabstraction::CDataType c_type,
+                               arrow::Type::type arrow_type) {
+  std::shared_ptr<arrow::Array> original_array;
+  arrow::ArrayFromVector<arrow::Time32Type, int32_t>(time32(arrow::TimeUnit::MILLI),
+                                                     input, &original_array);
+
+  auto converted_array = convertArray(original_array, c_type);
+
+  AssertConvertedArray(expected_array, converted_array, input.size(), arrow_type);
+}
+
+void TestTime64ArrayConversion(const std::vector<int64_t>& input,
+                               const std::shared_ptr<arrow::Array>& expected_array,
+                               odbcabstraction::CDataType c_type,
+                               arrow::Type::type arrow_type) {
+  std::shared_ptr<arrow::Array> original_array;
+  arrow::ArrayFromVector<arrow::Time64Type, int64_t>(time64(arrow::TimeUnit::NANO),
+                                                     input, &original_array);
+
+  auto converted_array = convertArray(original_array, c_type);
+
+  AssertConvertedArray(expected_array, converted_array, input.size(), arrow_type);
+}
+
+TEST(Utils, Time32ToTimeStampArray) {
+  std::vector<int32_t> input_data = {14896, 17820};
+
+  const auto seconds_from_epoch = odbcabstraction::GetTodayTimeFromEpoch();
+  std::vector<int64_t> expected_data;
+  expected_data.reserve(2);
+
+  for (const auto &item : input_data) {
+    expected_data.emplace_back(item + seconds_from_epoch * 1000);
+  }
+
+  std::shared_ptr<arrow::Array> expected;
+  auto timestamp_field = field("timestamp_field", timestamp(arrow::TimeUnit::MILLI));
+  arrow::ArrayFromVector<arrow::TimestampType, int64_t>(timestamp_field->type(),
+                                                        expected_data, &expected);
+
+  TestTime32ArrayConversion(input_data, expected,
+                            odbcabstraction::CDataType_TIMESTAMP,
+                            arrow::Type::TIMESTAMP);
+}
+
+TEST(Utils, Time64ToTimeStampArray) {
+  std::vector<int64_t> input_data = {1579489200000, 1646881200000};
+
+  const auto seconds_from_epoch = odbcabstraction::GetTodayTimeFromEpoch();
+  std::vector<int64_t> expected_data;
+  expected_data.reserve(2);
+
+  for (const auto &item : input_data) {
+    expected_data.emplace_back(item + seconds_from_epoch * 1000000000);
+  }
+
+  std::shared_ptr<arrow::Array> expected;
+  auto timestamp_field = field("timestamp_field", timestamp(arrow::TimeUnit::NANO));
+  arrow::ArrayFromVector<arrow::TimestampType, int64_t>(timestamp_field->type(),
+                                                        expected_data, &expected);
+
+  TestTime64ArrayConversion(input_data, expected,
+                            odbcabstraction::CDataType_TIMESTAMP,
+                            arrow::Type::TIMESTAMP);
 }
 
 TEST(Utils, StringToDateArray) {
-  TestArrayConversion({"2020-01-20", "2022-03-10"},
-                      odbcabstraction::CDataType_DATE, arrow::Type::DATE64);
+  std::shared_ptr<arrow::Array> expected;
+  arrow::ArrayFromVector<arrow::Date64Type, int64_t>(
+    {1579489200000, 1646881200000}, &expected);
+
+  TestArrayConversion({"2020-01-20", "2022-03-10"}, expected,
+                      odbcabstraction::CDataType_DATE,
+                      arrow::Type::DATE64);
 }
 
 TEST(Utils, StringToTimeArray) {
-  TestArrayConversion({"10:00", "12:00"},
-                      odbcabstraction::CDataType_TIME, arrow::Type::TIME64);
-}
+  std::shared_ptr<arrow::Array> expected;
+  arrow::ArrayFromVector<arrow::Time64Type, int64_t>(time64(arrow::TimeUnit::MICRO),
+                                                     {36000000000, 43200000000}, &expected);
 
-TEST(Utils, StringToTimeStampArray) {
-  TestArrayConversion({"2013-05-27 19:15:00", "2013-05-31 16:40:00"},
-                      odbcabstraction::CDataType_TIMESTAMP, arrow::Type::TIMESTAMP);
+  TestArrayConversion({"10:00", "12:00"}, expected,
+                      odbcabstraction::CDataType_TIME, arrow::Type::TIME64);
 }
 
 TEST(Utils, ConvertSqlPatternToRegexString) {
