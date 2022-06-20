b8f01401c475b46f2f3200aa4c79faa750961243
Rafael Telles <rafael.telles@dremio.com>
2022-06-20 14:32:17 -0300

    Fix NUMERIC data conversion
    Change-Id: I7edd9418fa42d967be2bf2d8b9eecfbbb0ea9ad9

--- flight_sql/utils.cc
+++ flight_sql/utils.cc
@@ -576,9 +576,11 @@ GetDefaultDataTypeForTypeId(arrow::Type::type type_id) {
       return arrow::time64(arrow::TimeUnit::MICRO);
     case arrow::Type::TIMESTAMP:
       return arrow::timestamp(arrow::TimeUnit::SECOND);
+    case arrow::Type::DECIMAL:
+      return arrow::decimal128(arrow::BasicDecimal128::kMaxPrecision, 0);
   }
 
-  throw odbcabstraction::DriverException(std::string("Invalid type id: ") + std::to_string(type_id));
+  throw odbcabstraction::DriverException(std::string("Invalid Arrow type id: ") + std::to_string(type_id));
 }
 
 arrow::Type::type
@@ -612,7 +614,7 @@ ConvertCToArrowType(odbcabstraction::CDataType data_type) {
     case odbcabstraction::CDataType_BINARY:
       return arrow::Type::BINARY;
     case odbcabstraction::CDataType_NUMERIC:
-      return arrow::Type::DECIMAL128;
+      return arrow::Type::DECIMAL;
     case odbcabstraction::CDataType_TIMESTAMP:
       return arrow::Type::TIMESTAMP;
     case odbcabstraction::CDataType_TIME:
@@ -660,6 +662,8 @@ odbcabstraction::CDataType ConvertArrowTypeToC(arrow::Type::type type_id) {
       return odbcabstraction::CDataType_TIME;
     case arrow::Type::TIMESTAMP:
       return odbcabstraction::CDataType_TIMESTAMP;
+    case arrow::Type::DECIMAL:
+      return odbcabstraction::CDataType_NUMERIC;
     default:
       throw odbcabstraction::DriverException(std::string("Invalid type id: ") + std::to_string(type_id));
   }
