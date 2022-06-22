4693d239b23fe78de269b7061e5b00a6357b6950
Vinicius Fraga <vinicius.fraga@dremio.com>
2022-06-22 18:25:32 -0300

    Add support for StringColumnLength when Precision/Scale is not available
    Change-Id: I26c806ce8c1c8f28ea34d1a5a8a1d710a86f5904

--- odbcabstraction/utils.cc
+++ odbcabstraction/utils.cc
@@ -34,5 +34,29 @@ bool AsBool(bool default_value, const Connection::ConnPropertyMap& connPropertyM
     return default_value;
   }
 }
+
+int32_t AsInt32(const std::string& value, int32_t default_value, int32_t min_value) {
+  const int32_t string_column_length = std::stoi(value);
+
+  if (string_column_length >= min_value && string_column_length <= INT32_MAX) {
+    return string_column_length;
+  } else {
+    return default_value;
+  }
+}
+
+int32_t AsInt32(int32_t default_value, int32_t min_value, const Connection::ConnPropertyMap& connPropertyMap,
+            const std::string& property_name) {
+
+  auto extracted_property = connPropertyMap.find(
+          property_name);
+
+  if (extracted_property != connPropertyMap.end()) {
+    return AsInt32(extracted_property->second, default_value, min_value);
+  }
+  else {
+    return default_value;
+  }
+}
 } // namespace odbcabstraction
 } // namespace driver
