6dd0135de4fb672aba64ad26e37936747b9c607b
Vinicius Fraga <vinicius.fraga@dremio.com>
2022-06-22 18:25:32 -0300

    Add support for StringColumnLength when Precision/Scale is not available
    Change-Id: I26c806ce8c1c8f28ea34d1a5a8a1d710a86f5904

--- flight_sql/flight_sql_statement.h
+++ flight_sql/flight_sql_statement.h
@@ -7,6 +7,7 @@
 #pragma once
 
 #include "flight_sql_statement_get_tables.h"
+#include "odbcabstraction/types.h"
 #include <odbcabstraction/spi/statement.h>
 #include <odbcabstraction/diagnostics.h>
 
@@ -26,6 +27,7 @@ private:
   arrow::flight::sql::FlightSqlClient &sql_client_;
   std::shared_ptr<odbcabstraction::ResultSet> current_result_set_;
   std::shared_ptr<arrow::flight::sql::PreparedStatement> prepared_statement_;
+  const odbcabstraction::MetadataSettings& metadata_settings_;
 
   std::shared_ptr<odbcabstraction::ResultSet>
   GetTables(const std::string *catalog_name, const std::string *schema_name,
@@ -36,7 +38,8 @@ public:
   FlightSqlStatement(
       const odbcabstraction::Diagnostics &diagnostics,
       arrow::flight::sql::FlightSqlClient &sql_client,
-      arrow::flight::FlightCallOptions call_options);
+      arrow::flight::FlightCallOptions call_options,
+      const odbcabstraction::MetadataSettings& metadata_settings);
 
   bool SetAttribute(StatementAttributeId attribute, const Attribute &value) override;
 
