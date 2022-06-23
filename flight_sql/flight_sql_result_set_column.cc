c0ccf3535397d22c879cafb3dea0b28a144207b8
Rafael Telles <rafael.telles@dremio.com>
2022-06-23 13:10:43 -0300

    Revert "Fix SQL_C_BINARY data conversions"
    This reverts commit 6bd50bc8e1a938eeaacd44f00d886dd14a8c4a5b.
    
    Change-Id: I3bcd5567c28e00ee305fdb73f876de475ef4abc8

--- flight_sql/flight_sql_result_set_column.cc
+++ flight_sql/flight_sql_result_set_column.cc
@@ -45,8 +45,7 @@ FlightSqlResultSetColumn::GetAccessorForTargetType(CDataType target_type) {
   const std::shared_ptr<arrow::Array> &original_array =
       result_set_->GetArrayForColumn(column_n_);
 
-  // When retrieving data as binary, its bytes should be the same as it's default type
-  if (target_type == odbcabstraction::CDataType_DEFAULT || target_type == odbcabstraction::CDataType_BINARY) {
+  if (target_type == odbcabstraction::CDataType_DEFAULT) {
     target_type = ConvertArrowTypeToC(original_array->type_id());
   }
 
