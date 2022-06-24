cd8b9f2bad184c09e6e39730eff800977c365b25
Jose Almeida <jose.almeida@dremio.com>
2022-06-24 17:24:31 -0300

    Try to get the CN from the ip
    - It adds a call to getaddrinfo, which tries to get the CN from the ip
    
    Change-Id: I403efd45f0397379ac6f78e82e02e44d9fb34d33

---
+++ flight_sql/address_info.cc
@@ -0,0 +1,35 @@
+/*
+ * Copyright (C) 2020-2022 Dremio Corporation
+ *
+ * See "LICENSE" for license information.
+ */
+
+#include "address_info.h"
+
+namespace driver {
+
+bool AddressInfo::GetAddressInfo(const std::string &host, char *host_name_info, int64_t max_host) {
+  if (result) {
+    freeaddrinfo(result);
+  }
+
+  int error;
+  error = getaddrinfo(host.c_str(), NULL, NULL, &result);
+
+  if (error != 0) {
+    return false;
+  }
+
+  error = getnameinfo(result->ai_addr, result->ai_addrlen, host_name_info,
+                          max_host, NULL, 0, 0);
+  return error == 0;
+}
+
+AddressInfo::~AddressInfo() {
+  if (result) {
+    freeaddrinfo(result);
+  }
+}
+
+AddressInfo::AddressInfo() : result(nullptr) {}
+} // namespace driver
