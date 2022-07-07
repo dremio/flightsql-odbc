3275c8435a1552a3f0cc93294b7f2c46de26dc93
Vinicius Fraga <vinicius.fraga@dremio.com>
2022-07-07 15:09:17 -0300

    Profiling Framework for ODBC
    Change-Id: I594ba8b77ff6e69c13bd597efd2a020db3cf5670

---
+++ odbc_perf_testing/test_strategy/execution_details.py
@@ -0,0 +1,40 @@
+#
+# Copyright (C) 2020-2022 Dremio Corporation
+#
+# See "LICENSE" for license information.
+
+from dataclasses import dataclass
+
+
+@dataclass
+class ConnectionDetails:
+    """
+    Class for storing connection information like host, port, user, password.
+    """
+    dsn: str
+    driver: str
+    host: str
+    port: int
+    user: str
+    password: str
+    use_encryption: bool
+    connected: bool = False
+
+
+@dataclass
+class TestDetails:
+    """
+    Class for storing test information like which ODBC library to use, and the SQL query that should run.
+    """
+    odbc_library: str
+    sql_query: str
+    test_case: str
+
+
+@dataclass
+class ExecutionDetails:
+    """
+    Class for storing the connection and test details.
+    """
+    connection_details: ConnectionDetails
+    test_details: TestDetails
