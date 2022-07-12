876682906de975b52acd209d9702b8037293f2b8
Jose Almeida <jose.almeida@dremio.com>
2022-07-12 17:11:57 -0300

    Implement logger interface
    - Add an interface for logger
    - Define Macro for each log level in the interface
    - Add an implementation from the logger interface
    - Add the spdlog dependency to the CMakeLists.txt
    
    Change-Id: Ibf1426c86e6e28903619cba328bd6998d4c4f74d

--- flight_sql/flight_sql_driver.cc
+++ flight_sql/flight_sql_driver.cc
@@ -5,14 +5,41 @@
  */
 
 #include "flight_sql_connection.h"
+#include "odbcabstraction/utils.h"
+#include <odbcabstraction/spd_logger.h>
 #include <odbcabstraction/platform.h>
+#include <odbcabstraction/utils.h>
 #include <flight_sql/flight_sql_driver.h>
+
+
+#define DEFAULT_MAXIMUM_FILE_SIZE 16777216
 
 namespace driver {
 namespace flight_sql {
 
 using odbcabstraction::Connection;
 using odbcabstraction::OdbcVersion;
+using odbcabstraction::LogLevel;
+using odbcabstraction::SPDLogger;
+
+namespace {
+  LogLevel ToLogLevel(int64_t level) {
+    switch (level) {
+      case 0:
+        return LogLevel::TRACE;
+      case 1:
+        return LogLevel::DEBUG;
+      case 2:
+        return LogLevel::INFO;
+      case 3:
+        return LogLevel::WARN;
+      case 4:
+        return LogLevel::ERROR;
+      default:
+        return LogLevel::OFF;
+    }
+  }
+}
 
 FlightSqlDriver::FlightSqlDriver()
     : diagnostics_("Apache Arrow", "Flight SQL", OdbcVersion::V_3),
@@ -31,5 +58,52 @@ odbcabstraction::Diagnostics &FlightSqlDriver::GetDiagnostics() {
 void FlightSqlDriver::SetVersion(std::string version) {
   version_ = std::move(version);
 }
+
+void FlightSqlDriver::RegisterLog(const std::string &configFileName) {
+  odbcabstraction::PropertyMap propertyMap;
+  driver::odbcabstraction::ReadConfigFile(propertyMap, configFileName);
+
+  auto log_enable_iterator = propertyMap.find(SPDLogger::LOG_ENABLED);
+
+  auto log_enabled = log_enable_iterator != propertyMap.end() ?
+    odbcabstraction::AsBool(log_enable_iterator->second) : false;
+
+  auto log_path_iterator = propertyMap.find(SPDLogger::LOG_PATH);
+
+  auto log_path =
+    log_path_iterator != propertyMap.end() ? log_path_iterator->second : "";
+
+  if (*log_enabled && !log_path.empty()) {
+    auto log_level_iterator = propertyMap.find(SPDLogger::LOG_LEVEL);
+
+    auto log_level =
+      log_level_iterator != propertyMap.end() ? std::stoi(log_level_iterator->second) : 1;
+
+    auto maximum_file_size_iterator = propertyMap.find(SPDLogger::MAXIMUM_FILE_SIZE);
+
+    auto maximum_file_size = maximum_file_size_iterator != propertyMap.end() ?
+      std::stoi(maximum_file_size_iterator->second) : DEFAULT_MAXIMUM_FILE_SIZE;
+
+    auto maximum_file_quantity_iterator = propertyMap.
+      find(SPDLogger::FILE_QUANTITY);
+
+    auto maximum_file_quantity =
+      maximum_file_quantity_iterator != propertyMap.end() ? std::stoi(
+        maximum_file_quantity_iterator->second) : 1;
+
+    std::unique_ptr<odbcabstraction::SPDLogger> logger (new odbcabstraction::SPDLogger());
+
+    logger->init(maximum_file_quantity, maximum_file_size,
+                                      log_path, ToLogLevel(log_level));
+    odbcabstraction::Logger::SetInstance(std::move(logger));
+  } else {
+    std::unique_ptr<odbcabstraction::SPDLogger> logger (new odbcabstraction::SPDLogger());
+
+    logger->init(1, 1,
+                 log_path, LogLevel::OFF);
+    odbcabstraction::Logger::SetInstance(std::move(logger));
+  }
+}
+
 } // namespace flight_sql
 } // namespace driver
