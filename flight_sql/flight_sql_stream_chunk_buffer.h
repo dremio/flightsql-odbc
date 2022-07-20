dc66c40e4c194f052befdff50ac363ccb5fb5e23
Rafael Telles <rafael.telles@dremio.com>
2022-07-20 17:09:38 -0300

    Implement double-buffering
    This implements buffering of FlightStreamChunks, allowing the driver to
    better leverage IO, as results are buffered as they arrive and can be
    consumed afterwards when the client requests them.
    This adds a new connection property "ChunkBufferCapacity" to set the
    buffer capacity with a default value of 5.
    
    Change-Id: I73454fc942f4b5fe8390ec4e582ea18b04eba9b4

---
+++ flight_sql/flight_sql_stream_chunk_buffer.h
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2020-2022 Dremio Corporation
+ *
+ * See "LICENSE" for license information.
+ */
+
+#pragma once
+
+#include <arrow/flight/client.h>
+#include <arrow/flight/sql/client.h>
+#include <odbcabstraction/blocking_queue.h>
+
+
+namespace driver {
+namespace flight_sql {
+
+using arrow::Result;
+using arrow::flight::FlightInfo;
+using arrow::flight::FlightStreamChunk;
+using arrow::flight::FlightStreamReader;
+using arrow::flight::sql::FlightSqlClient;
+using driver::odbcabstraction::BlockingQueue;
+
+class FlightStreamChunkBuffer {
+  BlockingQueue<Result<FlightStreamChunk>> queue_;
+
+public:
+  FlightStreamChunkBuffer(FlightSqlClient &flight_sql_client,
+                          const arrow::flight::FlightCallOptions &call_options,
+                          const std::shared_ptr<FlightInfo> &flight_info,
+                          size_t queue_capacity = 5);
+
+  ~FlightStreamChunkBuffer();
+
+  void Close();
+
+  bool GetNext(FlightStreamChunk* chunk);
+
+};
+
+}
+}
