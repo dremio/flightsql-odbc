9ce300e7aa84b25a93e08f4061263b89b4faf68e
Rafael Telles <rafael.telles@dremio.com>
2022-07-20 17:09:38 -0300

    WIP: Implement double-buffering
    Change-Id: I73454fc942f4b5fe8390ec4e582ea18b04eba9b4

---
+++ flight_sql/flight_sql_stream_chunk_buffer.h
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2020-2022 Dremio Corporation
+ *
+ * See "LICENSE" for license information.
+ */
+
+#pragma once
+
+#include <arrow/flight/client.h>
+#include <arrow/flight/sql/client.h>
+#include <odbcabstraction/blocking_queue.h>
+
+
+namespace driver {
+namespace flight_sql {
+
+using arrow::Result;
+using arrow::flight::FlightInfo;
+using arrow::flight::FlightStreamChunk;
+using arrow::flight::FlightStreamReader;
+using arrow::flight::sql::FlightSqlClient;
+using driver::odbcabstraction::BlockingQueue;
+
+class FlightStreamChunkBuffer {
+  BlockingQueue<Result<FlightStreamChunk>> queue_;
+  std::vector<std::shared_ptr<FlightStreamReader>> iterators_;
+
+public:
+  FlightStreamChunkBuffer(FlightSqlClient &flight_sql_client,
+                          const arrow::flight::FlightCallOptions &call_options,
+                          const std::shared_ptr<FlightInfo> &flight_info,
+                          size_t queue_capacity = 2);
+
+  ~FlightStreamChunkBuffer();
+
+  void Close();
+
+  bool GetNext(FlightStreamChunk* chunk);
+
+};
+
+}
+}
