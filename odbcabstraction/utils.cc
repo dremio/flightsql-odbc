8cb272e243dc7e169422ef2621178be29acf6982
Rafael Telles <rafael.telles@dremio.com>
2022-07-29 13:08:17 -0300

    Reapply "Implement logger interface"
    This reverts commit 0a310d64d5bdf0aa9d2922edda62b644a3d35251: Implement logger interface
    
    - Add an interface for logger
    - Define Macro for each log level in the interface
    - Add an implementation from the logger interface
    - Add the spdlog dependency to the CMakeLists.txt
    
    In addition to that this commit this also:
    
    - Fixes build on Windows
    - Changes log strategy to be lazy, so we can log strings without impact
      performance if logging is disabled
    
    Change-Id: If58d9c1ee7ea11390d858bd9815e6b9b11a9bee9

--- odbcabstraction/utils.cc
+++ odbcabstraction/utils.cc
@@ -5,8 +5,15 @@
  */
 
 #include <odbcabstraction/utils.h>
+#include "whereami.h"
+
+#include <fstream>
+#include <sstream>
 
 #include <boost/algorithm/string/predicate.hpp>
+#include <boost/tokenizer.hpp>
+#include <boost/token_functions.hpp>
+#include <iostream>
 
 namespace driver {
 namespace odbcabstraction {
@@ -43,6 +50,51 @@ boost::optional<int32_t> AsInt32(int32_t min_value, const Connection::ConnProper
     }
   }
   return boost::none;
+}
+
+std::string GetModulePath() {
+  std::vector<char> path;
+  int length, dirname_length;
+  length = wai_getModulePath(NULL, 0, &dirname_length);
+
+  if (length != 0) {
+    path.resize(length);
+    wai_getModulePath(path.data(), length, &dirname_length);
+  } else {
+    throw DriverException("Could not find module path.");
+  }
+
+  return std::string(path.begin(), path.begin() + dirname_length);
+}
+
+void ReadConfigFile(PropertyMap &properties, const std::string &config_file_name) {
+  auto config_path = GetModulePath();
+
+  std::ifstream config_file;
+  auto config_file_path = config_path + "/" + config_file_name;
+  config_file.open(config_file_path);
+
+  if (config_file.fail()) {
+    auto error_msg = "Arrow Flight SQL ODBC driver config file not found on \"" + config_file_path +  "\"";
+    std::cerr << error_msg << std::endl;
+
+    throw DriverException(error_msg);
+  }
+
+  std::string temp_config;
+
+  boost::char_separator<char> separator("=");
+  while(config_file.good()) {
+    config_file >> temp_config;
+    boost::tokenizer<boost::char_separator<char>> tokenizer(temp_config, separator);
+
+    auto iterator = tokenizer.begin();
+
+    std::string key = *iterator;
+    std::string value = *++iterator;
+
+    properties[key] = std::move(value);
+  }
 }
 
 } // namespace odbcabstraction
