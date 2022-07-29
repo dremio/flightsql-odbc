c67cb2067878f33ce38316ca7533dbfd32309209
Rafael Telles <rafael.telles@dremio.com>
2022-07-29 13:08:17 -0300

    Reapply "Implement logger interface"
    This reverts commit 0a310d64d5bdf0aa9d2922edda62b644a3d35251.
    
    Change-Id: If58d9c1ee7ea11390d858bd9815e6b9b11a9bee9

---
+++ odbcabstraction/spd_logger.cc
@@ -0,0 +1,154 @@
+/*
+ * Copyright (C) 2020-2022 Dremio Corporation
+ *
+ * See "LICENSE" for license information.
+ */
+
+#include "odbcabstraction/spd_logger.h"
+
+#include "odbcabstraction/logger.h"
+
+#include <spdlog/async.h>
+#include <spdlog/sinks/rotating_file_sink.h>
+#include <spdlog/spdlog.h>
+
+#include <cstdint>
+#include <csignal>
+
+namespace driver {
+namespace odbcabstraction {
+
+const std::string SPDLogger::LOG_LEVEL = "LogLevel";
+const std::string SPDLogger::LOG_PATH= "LogPath";
+const std::string SPDLogger::MAXIMUM_FILE_SIZE= "MaximumFileSize";
+const std::string SPDLogger::FILE_QUANTITY= "FileQuantity";
+const std::string SPDLogger::LOG_ENABLED= "LogEnabled";
+
+namespace {
+std::function<void(int)> shutdown_handler;
+void signal_handler(int signal) {
+  shutdown_handler(signal);
+}
+
+typedef void (*Handler)(int signum);
+
+Handler old_sigint_handler = SIG_IGN;
+Handler old_sigsegv_handler = SIG_IGN;
+Handler old_sigabrt_handler = SIG_IGN;
+#ifdef SIGKILL
+Handler old_sigkill_handler = SIG_IGN;
+#endif
+
+Handler GetHandlerFromSignal(int signum) {
+  switch (signum) {
+    case(SIGINT):
+      return old_sigint_handler;
+    case(SIGSEGV):
+      return old_sigsegv_handler;
+    case(SIGABRT):
+      return old_sigabrt_handler;
+#ifdef SIGKILL
+    case(SIGKILL):
+      return old_sigkill_handler;
+#endif
+  }
+}
+
+void SetSignalHandler(int signum) {
+  Handler old = signal(signum, SIG_IGN);
+  if (old != SIG_IGN) {
+    auto old_handler = GetHandlerFromSignal(signum);
+    old_handler = old;
+  }
+  signal(signum, signal_handler);
+}
+
+void ResetSignalHandler(int signum) {
+  Handler actual_handler = signal(signum, SIG_IGN);
+  if (actual_handler == signal_handler) {
+    signal(signum, GetHandlerFromSignal(signum));
+  }
+}
+
+
+spdlog::level::level_enum ToSpdLogLevel(LogLevel level) {
+  switch (level) {
+  case LogLevel_TRACE:
+    return spdlog::level::trace;
+  case LogLevel_DEBUG:
+    return spdlog::level::debug;
+  case LogLevel_INFO:
+    return spdlog::level::info;
+  case LogLevel_WARN:
+    return spdlog::level::warn;
+  case LogLevel_ERROR:
+    return spdlog::level::err;
+  default:
+    return spdlog::level::off;
+  }
+}
+} // namespace
+
+void SPDLogger::init(int64_t fileQuantity, int64_t maxFileSize,
+                     const std::string &fileNamePrefix, LogLevel level) {
+  logger_ = spdlog::rotating_logger_mt<spdlog::async_factory>(
+      "ODBC Logger", fileNamePrefix, maxFileSize, fileQuantity);
+
+  logger_->set_level(ToSpdLogLevel(level));
+
+  if (level != LogLevel::LogLevel_OFF) {
+    SetSignalHandler(SIGINT);
+    SetSignalHandler(SIGSEGV);
+    SetSignalHandler(SIGABRT);
+#ifdef SIGKILL
+    SetSignalHandler(SIGKILL);
+#endif
+    shutdown_handler = [&](int signal) {
+      logger_->flush();
+      spdlog::shutdown();
+      auto handler = GetHandlerFromSignal(signal);
+      handler(signal);
+    };
+  }
+}
+
+void SPDLogger::log(LogLevel level, const std::string &message) {
+  switch (level) {
+  case LogLevel_DEBUG:
+    logger_->debug(message);
+    break;
+  case LogLevel_TRACE:
+    logger_->trace(message);
+    break;
+  case LogLevel_INFO:
+    logger_->info(message);
+    break;
+  case LogLevel_WARN:
+    logger_->warn(message);
+    break;
+  case LogLevel_ERROR:
+    logger_->error(message);
+    break;
+  }
+}
+
+SPDLogger::~SPDLogger() {
+  ResetSignalHandler(SIGINT);
+  ResetSignalHandler(SIGSEGV);
+  ResetSignalHandler(SIGABRT);
+#ifdef SIGKILL
+  ResetSignalHandler(SIGKILL);
+#endif
+}
+
+bool SPDLogger::checkLogLevel(LogLevel called) {
+  auto set = logger_->level();
+  if (set == spdlog::level::off) {
+    return false;
+  } else {
+    return set <= ToSpdLogLevel(called);
+  }
+}
+
+} // namespace odbcabstraction
+} // namespace driver
