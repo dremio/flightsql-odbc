18d49fa5aa33f5912c808bf6c7353516d9964633
Rafael Telles <rafael.telles@dremio.com>
2022-07-29 13:08:17 -0300

    Reapply "Implement logger interface"
    This reverts commit 0a310d64d5bdf0aa9d2922edda62b644a3d35251: Implement logger interface
    
    - Add an interface for logger
    - Define Macro for each log level in the interface
    - Add an implementation from the logger interface
    - Add the spdlog dependency to the CMakeLists.txt
    
    In addition to that this commit this also:
    
    - Fixes build on Windows
    - Changes log strategy to be lazy, so we can log strings without impact
      performance if logging is disabled
    
    Change-Id: If58d9c1ee7ea11390d858bd9815e6b9b11a9bee9

--- flight_sql/flight_sql_driver.cc
+++ flight_sql/flight_sql_driver.cc
@@ -5,14 +5,41 @@
  */
 
 #include "flight_sql_connection.h"
+#include "odbcabstraction/utils.h"
+#include <odbcabstraction/spd_logger.h>
 #include <odbcabstraction/platform.h>
 #include <flight_sql/flight_sql_driver.h>
+
+
+#define DEFAULT_MAXIMUM_FILE_SIZE 16777216
+#define CONFIG_FILE_NAME "arrow-odbc.ini"
 
 namespace driver {
 namespace flight_sql {
 
 using odbcabstraction::Connection;
 using odbcabstraction::OdbcVersion;
+using odbcabstraction::LogLevel;
+using odbcabstraction::SPDLogger;
+
+namespace {
+  LogLevel ToLogLevel(int64_t level) {
+    switch (level) {
+      case 0:
+        return LogLevel::LogLevel_TRACE;
+      case 1:
+        return LogLevel::LogLevel_DEBUG;
+      case 2:
+        return LogLevel::LogLevel_INFO;
+      case 3:
+        return LogLevel::LogLevel_WARN;
+      case 4:
+        return LogLevel::LogLevel_ERROR;
+      default:
+        return LogLevel::LogLevel_OFF;
+    }
+  }
+}
 
 FlightSqlDriver::FlightSqlDriver()
     : diagnostics_("Apache Arrow", "Flight SQL", OdbcVersion::V_3),
@@ -31,5 +58,47 @@ odbcabstraction::Diagnostics &FlightSqlDriver::GetDiagnostics() {
 void FlightSqlDriver::SetVersion(std::string version) {
   version_ = std::move(version);
 }
+
+void FlightSqlDriver::RegisterLog() {
+  odbcabstraction::PropertyMap propertyMap;
+  driver::odbcabstraction::ReadConfigFile(propertyMap, CONFIG_FILE_NAME);
+
+  auto log_enable_iterator = propertyMap.find(SPDLogger::LOG_ENABLED);
+  auto log_enabled = log_enable_iterator != propertyMap.end() ?
+    odbcabstraction::AsBool(log_enable_iterator->second) : false;
+  if (!log_enabled) {
+    return;
+  }
+
+  auto log_path_iterator = propertyMap.find(SPDLogger::LOG_PATH);
+  auto log_path =
+    log_path_iterator != propertyMap.end() ? log_path_iterator->second : "";
+  if (log_path.empty()) {
+    return;
+  }
+
+  auto log_level_iterator = propertyMap.find(SPDLogger::LOG_LEVEL);
+  auto log_level =
+          ToLogLevel(log_level_iterator != propertyMap.end() ? std::stoi(log_level_iterator->second) : 1);
+  if (log_level == odbcabstraction::LogLevel_OFF) {
+    return;
+  }
+
+  auto maximum_file_size_iterator = propertyMap.find(SPDLogger::MAXIMUM_FILE_SIZE);
+  auto maximum_file_size = maximum_file_size_iterator != propertyMap.end() ?
+    std::stoi(maximum_file_size_iterator->second) : DEFAULT_MAXIMUM_FILE_SIZE;
+
+  auto maximum_file_quantity_iterator = propertyMap.find(SPDLogger::FILE_QUANTITY);
+  auto maximum_file_quantity =
+    maximum_file_quantity_iterator != propertyMap.end() ? std::stoi(
+      maximum_file_quantity_iterator->second) : 1;
+
+  std::unique_ptr<odbcabstraction::SPDLogger> logger(new odbcabstraction::SPDLogger());
+
+  logger->init(maximum_file_quantity, maximum_file_size,
+                                    log_path, log_level);
+  odbcabstraction::Logger::SetInstance(std::move(logger));
+}
+
 } // namespace flight_sql
 } // namespace driver
