60e6cd7b89ebb91f6bf4bbb65665a9ad82e469cc
Altay <altay.farkhadoglu@gmail.com>
2025-02-01 10:50:41 +0100

    hardcoded and enabled logs, because passing params from connector did not work

--- flight_sql/flight_sql_driver.cc
+++ flight_sql/flight_sql_driver.cc
@@ -4,11 +4,14 @@
  * See "LICENSE" for license information.
  */
 
+#include <boost/container/container_fwd.hpp>
+
+
+#include <flight_sql/flight_sql_driver.h>
+#include <odbcabstraction/platform.h>
+#include <odbcabstraction/spd_logger.h>
 #include "flight_sql_connection.h"
 #include "odbcabstraction/utils.h"
-#include <odbcabstraction/spd_logger.h>
-#include <odbcabstraction/platform.h>
-#include <flight_sql/flight_sql_driver.h>
 
 
 #define DEFAULT_MAXIMUM_FILE_SIZE 16777216
@@ -64,25 +67,26 @@ void FlightSqlDriver::RegisterLog() {
   driver::odbcabstraction::ReadConfigFile(propertyMap, CONFIG_FILE_NAME);
 
   auto log_enable_iterator = propertyMap.find(SPDLogger::LOG_ENABLED);
-  auto log_enabled = log_enable_iterator != propertyMap.end() ?
-    odbcabstraction::AsBool(log_enable_iterator->second) : false;
-  if (!log_enabled) {
-    return;
-  }
+  auto log_enabled =
+          log_enable_iterator != propertyMap.end() ? odbcabstraction::AsBool(log_enable_iterator->second) : false;
+
+  // if (!log_enabled) {
+  //   return;
+  // }
 
   auto log_path_iterator = propertyMap.find(SPDLogger::LOG_PATH);
-  auto log_path =
-    log_path_iterator != propertyMap.end() ? log_path_iterator->second : "";
+  std::string log_path = "D:\\arrow-odbc.log";
+    // log_path_iterator != propertyMap.end() ? log_path_iterator->second : "";
   if (log_path.empty()) {
     return;
   }
 
   auto log_level_iterator = propertyMap.find(SPDLogger::LOG_LEVEL);
-  auto log_level =
-          ToLogLevel(log_level_iterator != propertyMap.end() ? std::stoi(log_level_iterator->second) : 1);
-  if (log_level == odbcabstraction::LogLevel_OFF) {
-    return;
-  }
+  auto log_level = odbcabstraction::LogLevel_TRACE;
+          // ToLogLevel(log_level_iterator != propertyMap.end() ? std::stoi(log_level_iterator->second) : 1);
+  // if (log_level == odbcabstraction::LogLevel_OFF) {
+  //   return;
+  // }
 
   auto maximum_file_size_iterator = propertyMap.find(SPDLogger::MAXIMUM_FILE_SIZE);
   auto maximum_file_size = maximum_file_size_iterator != propertyMap.end() ?
