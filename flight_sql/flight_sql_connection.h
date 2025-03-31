a03ad02b426264a0b3e04432658914045dc077a3
Fernando Moreira <fernando.moreira@dremio.com>
2025-03-31 16:20:06 +0100

    DX-101630: Add `hideSQLTablesListing` flag (#10)
    When this flag is set to `true` the driver will always return empty
    results when an ODBC API call is made to the `SQLTables` function.
    Therefore avoiding the crash exhibited by MS Excel v16.95 when listing
    the available tables to be queried.
    
    Full context in
    [DX-101630](https://dremio.atlassian.net/browse/DX-101630).
    
    [DX-101630]:
    https://dremio.atlassian.net/browse/DX-101630?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- flight_sql/flight_sql_connection.h
+++ flight_sql/flight_sql_connection.h
@@ -62,6 +62,7 @@ public:
   static const std::string STRING_COLUMN_LENGTH;
   static const std::string USE_WIDE_CHAR;
   static const std::string CHUNK_BUFFER_CAPACITY;
+  static const std::string HIDE_SQL_TABLES_LISTING;
 
   explicit FlightSqlConnection(odbcabstraction::OdbcVersion odbc_version, const std::string &driver_version = "0.9.0.0");
 
@@ -107,6 +108,8 @@ public:
   bool GetUseWideChar(const ConnPropertyMap &connPropertyMap);
 
   size_t GetChunkBufferCapacity(const ConnPropertyMap &connPropertyMap);
+
+  bool GetHideSQLTablesListing(const ConnPropertyMap &connPropertyMap);
 };
 } // namespace flight_sql
 } // namespace driver
